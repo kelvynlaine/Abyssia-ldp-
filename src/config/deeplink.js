aae861d463ac0359d18fc9f8dbc24869022b02c3
Kelvyn Laine <kelvynwear@gmail.com>
2026-05-24 01:11:33 +0200

    chore: update Discord invitation link to https://discord.gg/QtG7jnkE5G

--- src/config/deeplink.js
+++ src/config/deeplink.js
@@ -8,7 +8,7 @@
 export const INSTALL_DEEPLINK_URL = "https://play.google.com/apps/testing/com.kelvyn.abyss_ia"; 
 
 // Lien alternatif de secours (ex: site officiel ou Discord)
-export const SUPPORT_DISCORD_URL = "https://discord.gg/abyssia";
+export const SUPPORT_DISCORD_URL = "https://discord.gg/QtG7jnkE5G";
 export const SUPPORT_EMAIL = "kelvynwear@gmail.com";
 export const SUPPORT_PHONE = "07 66 60 13 35";
 export const BRAND_EMAIL = "kelvynwear@gmail.com";
